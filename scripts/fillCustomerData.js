2c1540d58f037e5747b055b2ea93015e5346ea35
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-05-09 17:17:45 -0700

    Working on organizing the functions for initCustomerData and fillCustomerData addPointsForm.html: + new button to cancel *tryUpdateCustomerPoints: now errors when updeting customerBalance redirect the user to the scanner + event for DOMContentLoaded that calls initCustomerData
    *moved fillCustomerFormDataByCardId from initCustomerData.js to fillCustomerData.js
    
    initCustomerData.js: *moved the anonimous function of the DOMContentLoaded event
     to initCustomer()
    
    clientData.html: +DOMContentLoaded that calls initCustomerData

--- scripts/fillCustomerData.js
+++ scripts/fillCustomerData.js
@@ -1,3 +1,6 @@
+import { getCard } from './getCard.js';
+import { getCustomer } from './getCustomers.js';
+
 export async function fillCustomerFormData(customer) {
     sessionStorage.setItem("loyverseCustomerId", customer.loyverseCustomerId);
     document.getElementById("email").value = customer.email;
@@ -7,3 +10,22 @@ export async function fillCustomerFormData(customer) {
     document.getElementById("total_points").value = customer.totalPoints;
     document.getElementById("date_of_birth").value = customer.dateOfBirth;
 }
+
+export async function fillCustomerFormDataByCardId(card_id) {
+    const card = await getCard(card_id);
+    if (card == null) {
+      alert("No se encontro ninguna tarjeta con el QR escaneado");
+      return undefined;
+    }
+    if(card.customerEmail == null) {
+      alert("La tarjeta escaneada no a sido enlazada a ningun cliente");
+      return null
+    }
+    const customer = await getCustomer(card.customerEmail);
+    if(customer == null) {
+      alert("Error inesperado: no se encontro el cliente enlazado con esta tarjeta");
+      return undefined;
+    }
+    fillCustomerFormData(customer);
+    return "Sueccess";
+  }
--- scripts/scanCard/initCustomerData.js
+++ scripts/scanCard/initCustomerData.js
@@ -1,32 +1,11 @@
-import { fillCustomerFormData } from '../fillCustomerData.js';
-import { getCard } from '../getCard.js';
-import { getCustomer } from '../getCustomers.js';
+import { fillCustomerFormDataByCardId } from '../fillCustomerData.js';
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const card_id = sessionStorage.getItem('scannedCardCode');
-    if (!card_id) {
-      alert('No hay QR escaneado');
-      returnToScanCard();
-    }
-    if (await fillCustomerFormDataByCardId(card_id) == undefined)
-      returnToScanCard();
-});
-
-async function fillCustomerFormDataByCardId(card_id) {
-    const card = await getCard(card_id);
-    if (card == null) {
-      alert("No se encontro ninguna tarjeta con el QR escaneado");
-      return undefined;
-    }
-    if(card.customerEmail == null) {
-      alert("La tarjeta escaneada no a sido enlazada a ningun cliente");
-      return null
-    }
-    const customer = await getCustomer(card.customerEmail);
-    if(customer == null) {
-      alert("Error inesperado: no se encontro el cliente enlazado con esta tarjeta");
-      return undefined;
-    }
-    fillCustomerFormData(customer);
-    return "Sueccess";
-  }
+export async function initCustomerData(returnPathOnError) {
+const card_id = sessionStorage.getItem('scannedCardCode');
+if (!card_id) {
+  alert('No hay QR escaneado');
+  window.location.href = returnPathOnError;
+}
+if (await fillCustomerFormDataByCardId(card_id) == undefined)
+    window.location.href = returnPathOnError;
+}
